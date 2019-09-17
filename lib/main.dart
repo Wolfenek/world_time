6682351cccb921e093deeeffabfdc5e1fe115621
Wolfenek <wolfenek@gmail.com>
2019-09-17 15:09:59 +0200

    established connection with API

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,8 @@ import 'package:world_time/pages/home.dart';
 import 'package:world_time/pages/loading.dart';
 import 'package:world_time/pages/choose_location.dart';
 
-void main() => runApp(MaterialApp(
-  initialRoute: '/home',
-  routes: {
-    '/': (context) => Loading(),
-    '/home': (context) => Home(),
-    '/location': (context) => ChooseLocation(),
-  }
-));
-
+void main() => runApp(MaterialApp(initialRoute: '/', routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    }));
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,33 +8,39 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
-    // simulate network request for a username
-    // await Future.delayed(Duration(seconds: 3), () => print('pepega'));
-    // await Future.delayed(Duration(seconds: 2), () {
-    //   print('Luigi');
-    // });
-    // print('statement');
-    String username = await Future.delayed(Duration(seconds: 2), () {
-      return 'Yoshi';
-    });
-    int age = await Future.delayed(Duration(seconds: 3), () {
-      return 25;
-    });
+  void getTime() async {
+    // make the request
+    Response response =
+        await get('http://worldtimeapi.org/api/timezone/Europe/London');
+    Map data = json.decode(response.body);
+    //print(data);
 
-    print('$username - $age years old');
+    // get properties from json
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    //print(datetime);
+    //print(offset);
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
     // Print below runs first. There's no await keyword in front of it
     print('hey there');
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('loading screen'));
+    return Scaffold(
+      body: SafeArea(
+        child: Text('hello'),
+      ),
+    );
   }
 }
