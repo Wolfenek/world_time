1139351eb8bde46de5599fb1cbf40750eb90b685
Wolfenek <wolfenek@gmail.com>
2019-09-20 10:43:45 +0200

    extracted the WorldTime Class

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -8,38 +7,28 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getTime() async {
-    // make the request
-    Response response =
-        await get('http://worldtimeapi.org/api/timezone/Europe/London');
-    Map data = json.decode(response.body);
-    //print(data);
+  String currentTime = 'loading';
 
-    // get properties from json
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    //print(datetime);
-    //print(offset);
-
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
+  void setupWorldTime() async {
+    WorldTime instance =
+        WorldTime(location: 'Warsaw', flag: 'poland.png', url: 'Europe/Warsaw');
+    await instance.getTime();
+    setState(() {
+      currentTime = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getTime();
-    // Print below runs first. There's no await keyword in front of it
-    print('hey there');
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Text('hello'),
+        child: Text(currentTime),
       ),
     );
   }
